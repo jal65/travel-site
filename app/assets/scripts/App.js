6da98a4f1e969653a4610916d3d52e98bf07054e
Jose Lourenco <jose@lourenco.ws>
2016-11-04 21:44:19 +0000

    weback intro

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,13 +1,14 @@
-function Person(fullName, favColor) {
-  this.name = fullName;
-  this.favoriteColor = favColor;
-  this.greet = function() {
-    console.log("Hello, my name is " + this.name + " and my favorite color is " + this.favoriteColor + ".");
-  }
-}
+var $ = require('jquery');
+
+var Person = require('./modules/Person');
+
+console.log(Person);
+
 
 var john = new Person("John Doe", "blue");
 john.greet();
 
 var jane = new Person("Jane Smith", "green");
 jane.greet();
+
+$("h1").remove();
