9f7ed9fdf41aadd280c0110d1ebb783fa9fd21f2
Jose Lourenco <jose@lourenco.ws>
2016-11-05 18:42:30 +0000

    sticky header all done

--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -9,24 +9,23 @@ class RevealOnScroll {
     this.createWaypoints();
   }
 
-  hideInitially () {
+  hideInitially() {
     this.itemsToReveal.addClass("reveal-item");
   }
 
   createWaypoints() {
     var that = this;
-    this.itemsToReveal.each(function(){
+    this.itemsToReveal.each(function() {
       var currentItem = this;
       new Waypoint({
         element: currentItem,
-        handler: function () {
+        handler: function() {
           $(currentItem).addClass("reveal-item--is-visible");
         },
         offset: that.offsetPercentage
       });
     });
   }
-
 }
 
 export default RevealOnScroll;
